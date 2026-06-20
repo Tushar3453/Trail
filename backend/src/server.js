ea41978037447d4a175e0f8308f30b67e087fe2a
Tushar Chaudhary <chaudharytushar270@gmail.com>
2026-06-20 20:16:07 +0530

    setup react frontend and express backend

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Trail API running");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
